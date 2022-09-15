e687b762f563788b8c90da7a3089bdbcb90d4c00
Yannick Lansink <76790689+yannicklansink@users.noreply.github.com>
2022-09-15 14:30:57 +0200

    setup new voetbal vereniging project
    setup for new voetbal vereniging project with spring.

---
+++ src/main/java/nl/belastingdienst/voetbal_vereniging/VoetbalVerenigingApplication.java
@@ -0,0 +1,13 @@
+package nl.belastingdienst.voetbal_vereniging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoetbalVerenigingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoetbalVerenigingApplication.class, args);
+    }
+
+}
